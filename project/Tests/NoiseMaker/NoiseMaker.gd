25cd54554409d3a742582766c328c99027c0818e
SirDuck145 <matthiasharden@gmail.com>
2020-10-23 14:52:27 -0700

    Add audio playing from location of noise, dampens audio as distance increases.

--- project/Tests/NoiseMaker/NoiseMaker.gd
+++ project/Tests/NoiseMaker/NoiseMaker.gd
@@ -1,8 +1,10 @@
-extends Node2D
+extends AudioStreamPlayer2D
 
 var test_location = Vector2(100, 150)
 var test_volume = 100
 var space_state
+var volume_scale = 1000
+var volume_percentage
 
 func calculate_distance(location, target_location):
 	var y = abs(target_location.y - location.y)
@@ -10,15 +12,21 @@ func calculate_distance(location, target_location):
 	var distance = sqrt(x*x + y*y)
 	return distance
 
-func make_noise(volume):
+func make_noise(volume=1):
 	var hearing_creatures = get_tree().get_nodes_in_group("hearing")
 	var heard = []
+	
+	# Play the noise
+	#$AudioStreamPlayer2D.set_stream(audio)
+	set_max_distance(volume*volume_scale)
+	play()
+	
 	for creature in hearing_creatures:
 		var target_location = Vector2(creature.position.x, creature.position.y)
 		var distance = calculate_distance(position, target_location)
 		
 		# Check all creatues in distance to hear noise
-		if distance <= volume:
+		if distance <= get_max_distance():
 			# For each creature in range, do a raycast from location to their location
 			var result = space_state.intersect_ray(position, target_location)
 			# every wall hit reduces volume level
@@ -27,9 +35,9 @@ func make_noise(volume):
 			# compare if volume is still greater than distance
 			if distance <= volume:
 				# call "hear" on creature with calculated volume and location
-				volume -= distance
+				volume_percentage = (distance/get_max_distance())
 				if creature.has_method("hear_noise"):
-					creature.hear_noise(volume, position)
+					creature.hear_noise(volume_percentage, position)
 	
 func _physics_process(delta):
 	space_state = get_world_2d().direct_space_state
